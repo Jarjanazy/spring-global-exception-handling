d95b20c3273d765747aa2f64174b8d5d7a30d3ae
cercenazi <celil.cercenazi@gmail.com>
2021-08-01 14:17:28 +0300

    add the initial spring boot project

---
+++ spring-global-exception-handle/src/main/java/jalil/demo/springglobalexceptionhandle/SpringGlobalExceptionHandleApplication.java
@@ -0,0 +1,13 @@
+package jalil.demo.springglobalexceptionhandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGlobalExceptionHandleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGlobalExceptionHandleApplication.class, args);
+	}
+
+}
